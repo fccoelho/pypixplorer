64f3983e4aceb57006e73f2ec937a794302b6ead
bruno cuconato <odanoburu@users.noreply.github.com>
2017-01-25 15:54:40 -0200

    change cache-ing profile of get_dependencies
    * divided function in two: get_dependencies and make_dep_json
    * before cache was made at every call; now it is made if it doesn't exist or if the package being search for is not there (as it may have been installed since the last call)

--- pypixplore/local.py
+++ pypixplore/local.py
@@ -25,38 +25,56 @@ class InstalledPackages:
 
     def upgradeable(self):
         raise NotImplementedError
+        
+    def make_dep_json(self):
+    """
+    Get the dependencies of all packages installed and cache the result in a json.
+        :return: a tinydb database
+    """
+        deptree = subprocess.getoutput('pipdeptree -j') # run pipdeptree (python module) on the terminal: outputs json
+        pack_json = json.loads(deptree) # load json to python environment
+
+        pack_db = TinyDB("pack_db.json")
+        pack_db.purge()  # the method clears the database on every call, avoiding rewrites of packages (duplicates)
+        pack_db.insert_multiple(pack_json)
+        return pack_db
 
     def get_dependencies(self, package_name):
         """
         Get the dependencies of a given installed package
         :param package_name: name of the package
-        :return: a dictionary of dependencies and their versions
+        :return: a dictionary of dependencies and their versions, or a string saying package is not installed
         """
-        deptree = subprocess.getoutput('pipdeptree -j')
-        pack_json = json.loads(deptree)
+        my_file = Path("pack_db.json")
+            if not my_file.is_file(): # test if cache exists
+                pack_db = self.make_dep_json() # make cache
+                Pack = Query()
+                list_version = pack_db.search(Pack.package.package_name == str(package_name)) # query cache for package
+            else:
+                pack_db = TinyDB("pack_db.json")
+                Pack = Query()
+                list_version = pack_db.search(Pack.package.package_name == str(package_name))
+                if len(list_version) == 0: # test if package is in cache
+                    pack_db = self.make_dep_json() # update cache because package may have been installed in the meantime
+                    #Pack = Query() #no need
+                    list_version = pack_db.search(Pack.package.package_name == str(package_name))
 
-        pack_db = TinyDB("pack_db.json")
-        pack_db.purge()  # the method clears the database on every call, avoiding rewrites of packages
-        pack_db.insert_multiple(pack_json)
+            if len(list_version) == 0:
+                return "Non installed package -- {}".format(str(package_name))
+            elif len(list_version) == 1:
+                deps = list_version[0]
+            else: #check which version is latest
+                max_idx, max_ver = 0, '0'
+                for idx, dic in enumerate(list_version):
+                    version = dic["package"]["installed_version"]
+                if lsvrs(version) > lsvrs(max_ver):
+                    max_idx, max_ver = idx, version
+                deps = list_version[max_idx]
+            deps_dict = {}
+            for dependency in deps['dependencies']: # changing output to dict
+                deps_dict[dependency['package_name']] = {'required_version': dependency['required_version'], 'installed_version': dependency['installed_version']}
 
-        Pack = Query()
-        list_version = pack_db.search(Pack.package.package_name == str(package_name))
-
-        if len(list_version) == 0:
-            return "Non installed package -- {}".format(str(package_name))
-        elif len(list_version) == 1:
-            deps = list_version[0]  # still need to improve the output
-        else:
-            max_idx, max_ver = 0, '0'
-            for idx, dic in enumerate(list_version):
-                version = dic["package"]["installed_version"]
-            if lsvrs(version) > lsvrs(max_ver):
-                max_idx, max_ver = idx, version
-            deps = list_version[max_idx]
-        deps_dict = {}
-        for dependency in deps['dependencies']:
-            deps_dict[dependency['package_name']] = {'required_version': dependency['required_version'], 'installed_version': dependency['installed_version']}
-        return deps_dict#, list(deps_dict.keys())
+            return deps_dict#, list(deps_dict.keys())
 
     def dependency_graph(self, package_name):
         raise NotImplementedError
