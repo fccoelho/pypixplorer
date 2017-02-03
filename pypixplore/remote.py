2ae9dc332d34ff76803285e2fc67173f5f1becb5
Flávio Codeço Coelho <fccoelho@gmail.com>
2017-02-03 14:10:06 -0200

    minor release

--- pypixplore/remote.py
+++ pypixplore/remote.py
@@ -1,5 +1,5 @@
 import xmlrpc.client as xmlrpcclient
-from tinydb import TinyDB, Query
+# from tinydb import TinyDB, Query
 import datetime
 import time
 import json
@@ -29,7 +29,7 @@ class Index:
         :param package_name: name of the package
         :return: dictionary
         """
-        Package = Query()
+        # Package = Query()
         # results = self.cache.search(Package.info.name == package_name)
 
         results = self.cache.get(package_name, None)
@@ -186,7 +186,7 @@ class Index:
             print('String expected as input')
             raise AttributeError
 
-        *useless, user, repo = hyperlink.split('/')
+
 
         return 'https://api.github.com/repos/{}/{}/'.format(user, repo)
     """
